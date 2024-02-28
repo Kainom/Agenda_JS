83f0985ea697b2e9db09654f6b93e841cdad8c71
Kainom <eleuteriokaina@gmail.com>
2024-02-27 21:53:15 -0300

    Criei algumas rotas e personalizei seus middle adjunto de uma pequena     estilização

--- routes.js
+++ routes.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const cadastroController = require('./src/controllers/cadastroController');
+const perfilController = require('./src/controllers/perfilController');
 
 // Rotas da home
-route.get('/', homeController.paginaInicial);
+route.get('/', homeController.index);
 
 
+//Rotas de login
+
+route.get('/login/index',loginController.index);
+
+
+//rotas de cadastro
+route.get('/cadastro/index',cadastroController.index);
+
+
+//rotas do perfil
+
+route.get('/perfil/index',perfilController.index);
 
 module.exports = route;
